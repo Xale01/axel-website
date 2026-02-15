9e27e485e18530ea126022f170df9db3bfd67974
AxelAvecDuSel <axelbenard01@gmail.com>
2026-02-15 22:33:45 +0100

    feat: add AppController for better control of routes and views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [AppController::class, 'index']);
